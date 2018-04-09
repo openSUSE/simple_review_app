a2ede0789d506acbb1afea10fe4705529aee5b9d
Christian Bruckmayer <cbruckmayer@suse.com>
2018-04-09 16:48:00 +0200

    Precompile assets for faster page load

--- files/contrib/entrypoint.sh
+++ files/contrib/entrypoint.sh
@@ -4,4 +4,5 @@ rm -rf tmp/pids/server.pid
 rm -rf log/development.sphinx.pid
 bundle install
 RAILS_ENV=development bundle exec rake dev:bootstrap
+RAILS_ENV=development bundle exec rake assets:precompile
 foreman start
