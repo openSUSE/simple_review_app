65b5016d1f49f6f4fbedf16088610fd9ce381ca2
Christian Bruckmayer <cbruckmayer@suse.com>
2018-04-09 16:11:00 +0200

    Only prepare development environment
    we do not need to prepare test.

--- files/contrib/entrypoint.sh
+++ files/contrib/entrypoint.sh
@@ -3,5 +3,5 @@ echo "Preparing application..."
 rm -rf tmp/pids/server.pid
 rm -rf log/development.sphinx.pid
 bundle install
-bundle exec rake dev:bootstrap
+RAILS_ENV=development bundle exec rake dev:bootstrap
 foreman start
