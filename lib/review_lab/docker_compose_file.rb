259984559c0e7f3d8532971c1d49cbd31fbf46ea
Christian Bruckmayer <cbruckmayer@suse.com>
2018-04-09 23:55:15 +0200

    Move DockerComposeFile into ReviewLab namespace

--- lib/review_lab/docker_compose_file.rb
+++ lib/review_lab/docker_compose_file.rb
@@ -4,52 +4,54 @@ require 'yaml'
 require 'active_model'
 require './lib/review_lab/logger'
 
-class DockerComposeFile
-  include ActiveModel::Model
-  include Logger
-  attr_accessor :path, :service_name
-  attr_writer :content
+class ReviewLab
+  class DockerComposeFile
+    include ActiveModel::Model
+    include Logger
+    attr_accessor :path, :service_name
+    attr_writer :content
 
-  def content
-    @content ||= YAML.load_file(docker_compose_file_path)
-  end
-
-  def save
-    File.open(path, 'w') do |f|
-      f.write(YAML.dump(content))
+    def content
+      @content ||= YAML.load_file(docker_compose_file_path)
     end
-  end
 
-  def set_review_app_information
-    add_traefik_frontend_rule(compose_file)
-    add_relative_url_root(compose_file)
-    save
-  end
+    def save
+      File.open(path, 'w') do |f|
+        f.write(YAML.dump(content))
+      end
+    end
 
-  private
+    def set_review_app_information
+      add_traefik_frontend_rule(compose_file)
+      add_relative_url_root(compose_file)
+      save
+    end
 
-  def service
-    content['services'][service_name]
-  end
+    private
 
-  def add_relative_url_root
-    logger.info "Set '#{root_url}' in docker-compose.yml file."
-    service['environment'] ||= []
-    service['environment'] << root_url
-    content
-  end
+    def service
+      content['services'][service_name]
+    end
 
-  def root_url
-    "RAILS_RELATIVE_URL_ROOT=/#{name}"
-  end
+    def add_relative_url_root
+      logger.info "Set '#{root_url}' in docker-compose.yml file."
+      service['environment'] ||= []
+      service['environment'] << root_url
+      content
+    end
 
-  def add_traefik_frontend_rule
-    logger.info "Set '#{traefik_frontend_rule}' in docker-compose.yml file."
-    service['labels']['traefik.frontend.rule'] = traefik_frontend_rule
-    content
-  end
+    def root_url
+      "RAILS_RELATIVE_URL_ROOT=/#{name}"
+    end
 
-  def traefik_frontend_rule
-    "Host:#{host}; PathPrefix:/#{name}"
+    def add_traefik_frontend_rule
+      logger.info "Set '#{traefik_frontend_rule}' in docker-compose.yml file."
+      service['labels']['traefik.frontend.rule'] = traefik_frontend_rule
+      content
+    end
+
+    def traefik_frontend_rule
+      "Host:#{host}; PathPrefix:/#{name}"
+    end
   end
 end
