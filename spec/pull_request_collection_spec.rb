8b90eeaab33a0af459965d313e147e7cf638fecd
Christian Bruckmayer <cbruckmayer@suse.com>
2018-05-21 15:05:44 +0200

    Implement specs for PullRequestCollection

---
+++ spec/pull_request_collection_spec.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..', 'lib')
+require 'octokit'
+require 'simple_review_app/pull_request_collection'
+
+describe SimpleReviewApp::PullRequestCollection, vcr: true do
+  let(:logger) { double }
+  let(:label) { 'review-lab' }
+
+  before do
+    allow(logger).to receive(:info)
+  end
+
+  describe '#all' do
+    subject do
+      SimpleReviewApp::PullRequestCollection.new(
+        repository: 'open-build-service',
+        organization: 'ChrisBr',
+        labels: label,
+        client: Octokit::Client.new,
+        logger: logger
+      ).all
+    end
+    let(:pr_label) { subject.first.content.labels.first.name }
+
+    it { expect(subject.length).to eq(1) }
+    it { expect(pr_label).to eq(label) }
+  end
+end
