21e33db865d4fa1c0d54f05bf06a9878a5a64f29
Christian Bruckmayer <cbruckmayer@suse.com>
2018-05-21 15:05:00 +0200

    Add VCR gem to test and record HTTP requests

---
+++ spec/support/vcr.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'vcr'
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/cassettes'
+  config.hook_into :webmock
+  config.default_cassette_options = { record: :new_episodes }
+  # config.allow_http_connections_when_no_cassette = true
+  config.configure_rspec_metadata!
+end
